e288e23e51789db5079f690036c1de033da1ccf1
Jose Mamani <jjvmamanirojas@gmail.com>
2019-01-04 00:09:37 -0600

    Update media query for index.html

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -35,21 +35,6 @@ initMap = () => {
   });
 }  
  
-/* window.initMap = () => {
-  fetchRestaurantFromURL((error, restaurant) => {
-    if (error) { // Got an error!
-      console.error(error);
-    } else {
-      self.map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16,
-        center: restaurant.latlng,
-        scrollwheel: false
-      });
-      fillBreadcrumb();
-      DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
-    }
-  });
-} */
 
 /**
  * Get current restaurant from page URL.
@@ -150,18 +135,22 @@ createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
   name.innerHTML = review.name;
+  name.classList.add('review-name')
   li.appendChild(name);
-
+  
   const date = document.createElement('p');
   date.innerHTML = review.date;
+  date.classList.add('review-date')
   li.appendChild(date);
-
+  
   const rating = document.createElement('p');
-  rating.innerHTML = `Rating: ${review.rating}`;
+  rating.innerHTML = `Rating: ${review.rating}/5`;
+  rating.classList.add('review-rating')
   li.appendChild(rating);
-
+  
   const comments = document.createElement('p');
   comments.innerHTML = review.comments;
+  comments.classList.add('review-comments')
   li.appendChild(comments);
 
   return li;
