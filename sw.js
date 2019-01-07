44738042c1f48b579ddeff3bea4e4b53d160f165
Jose Mamani <jjvmamanirojas@gmail.com>
2019-01-07 10:59:46 -0600

    Add sw.js and finish making the application responsive

---
+++ sw.js
@@ -0,0 +1,52 @@
+const staticCache = 'resto-cache';
+
+self.addEventListener('activate', event => {
+  event.waitUntil( 
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName.startsWith('resto-') && cacheName !== staticCache
+        }).map(cacheName => {
+          return caches.delete(cacheName)
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(staticCache).then(cache => {
+      return cache.addAll( /
+        [ 
+          '/index.html',
+          '/restaurant.html',
+          '/data/restaurants.json',
+          '/js/main.js',
+          '/js/main.js',
+          '/js/restaurant_info.js',
+          '/css/styles.css',
+          '/img/1.jpg',
+          '/img/2.jpg',
+          '/img/3.jpg',
+          '/img/4.jpg',
+          '/img/5.jpg',
+          '/img/6.jpg',
+          '/img/7.jpg',
+          '/img/8.jpg',
+          '/img/9.jpg',
+          '/img/10.jpg',
+          '/'
+        ]
+      )
+    })
+  )
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request)
+    })
+  )
+})
